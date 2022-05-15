1935b91f8b218cf49babec2a9a462e1ce1a5f8f0
Masen Furer <m_github@0x26.net>
2022-05-15 10:59:09 -0700

    test_kiss_util: migrate from unittest to pytest

--- tests/test_kiss_util.py
+++ tests/test_kiss_util.py
@@ -3,55 +3,30 @@
 
 """Tests for KISS Util Module."""
 
-import unittest
-
-from .context import kiss3  # pylint: disable=R0801
-from .context import kiss_test_classes  # pylint: disable=R0801
-
-from . import constants  # pylint: disable=R0801
+import kiss3
 
 __author__ = 'Greg Albrecht W2GMD <oss@undef.net>'  # NOQA pylint: disable=R0801
 __copyright__ = 'Copyright 2017 Greg Albrecht and Contributors'  # NOQA pylint: disable=R0801
 __license__ = 'Apache License, Version 2.0'  # NOQA pylint: disable=R0801
 
 
-class KISSUtilTestCase(kiss_test_classes.KISSTestClass):  # NOQA pylint: disable=R0904,C0103
-
-    """Test class for KISS Python Module."""
-
-    def setUp(self):
-        """Setup."""
-        self.test_frames = open(constants.TEST_FRAMES, 'rb')
-        self.test_frame = self.test_frames.readlines()[0].strip()
-
-    def tearDown(self):
-        """Teardown."""
-        self.test_frames.close()
-
-    def test_escape_special_codes_fend(self):
-        """
-        Tests `kiss3.escape_special_codes` util function.
-        """
-        fend = kiss3.escape_special_codes(kiss3.FEND)
-        self._logger.debug('fend=%s', fend)
-        self.assertEqual(fend, kiss3.FESC_TFEND)
-
-    def test_escape_special_codes_fesc(self):
-        """
-        Tests `kiss3.escape_special_codes` util function.
-        """
-        fesc = kiss3.escape_special_codes(kiss3.FESC)
-        self._logger.debug('fesc=%s', fesc)
-        self.assertEqual(fesc, kiss3.FESC_TFESC)
-
-    def test_extract_ui(self):
-        """
-        Tests `kiss3.extract_ui` util function.
-        """
-        frame_ui = kiss3.extract_ui(self.test_frame)
-        self._logger.debug('frame_ui=%s', frame_ui)
-        self.assertEqual('APRX240W2GMD 6WIDE1 1', frame_ui)
+def test_escape_special_codes_fend():
+    """
+    Tests `kiss3.escape_special_codes` util function.
+    """
+    assert kiss3.escape_special_codes(kiss3.FEND) == kiss3.FESC_TFEND
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_escape_special_codes_fesc():
+    """
+    Tests `kiss3.escape_special_codes` util function.
+    """
+    assert kiss3.escape_special_codes(kiss3.FESC) == kiss3.FESC_TFESC
+
+
+def test_extract_ui(sample_frames):
+    """
+    Tests `kiss3.extract_ui` util function.
+    """
+    frame_ui = kiss3.extract_ui(sample_frames[0])
+    assert frame_ui == 'APRX240W2GMD 6WIDE1 1'
