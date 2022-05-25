1171cb9f072dc9cc2a3a7391211671c6b75c9ddc
Masen Furer <m_github@0x26.net>
2022-05-24 17:13:04 -0700

    Clean up package public api __all__

--- kiss3/__init__.py
+++ kiss3/__init__.py
@@ -1,10 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Python KISS Module.
-
 """
-Python KISS Module.
+kiss3 Python KISS Module.
 ~~~~
 
 
@@ -12,61 +7,18 @@ Python KISS Module.
 :copyright: Copyright 2017 Greg Albrecht and Contributors
 :license: Apache License, Version 2.0
 :source: <https://github.com/ampledata/kiss>
-
 """
 from importlib_metadata import version
 
-from .ax25 import (  # noqa: F401
+from .ax25 import (
     Address,
     Control,
     Frame,
     FrameType,
 )
-from .classes import KISS, SerialKISS, TCPKISS  # noqa: F401
-from .constants import (  # noqa: F401
-    LOG_FORMAT,
-    LOG_LEVEL,
-    SERIAL_TIMEOUT,
-    READ_BYTES,  # NOQA
-    FEND,
-    FESC,
-    TFEND,
-    TFESC,
-    FESC_TFEND,
-    FESC_TFESC,
-    DATA_FRAME,
-    TX_DELAY,
-    PERSISTENCE,
-    SLOT_TIME,
-    TX_TAIL,
-    FULL_DUPLEX,
-    SET_HARDWARE,
-    RETURN,
-    DATAFRAME,
-    TXDELAY,
-    P,
-    SLOTTIME,
-    TXTAIL,
-    FULLDUPLEX,
-    SETHARDWARE,
-    DEFAULT_KISS_CONFIG_VALUES,
-    KISS_ON,
-    KISS_OFF,
-    NMEA_HEADER,
-    NO_PROTOCOL_ID,
-)
-
-from .exceptions import SocketClosetError  # NOQA
-
-from .util import (  # noqa: F401
-    escape_special_codes,
-    recover_special_codes,
-    extract_ui,  # NOQA
-    strip_df_start,
-    strip_nmea,
-)
-
-from .classes import KISS, TCPKISS, SerialKISS  # NOQA
+from .classes import AbstractKISS, SerialKISS, TCPKISS
+from .kiss import create_serial_connection, create_tcp_connection, KISSProtocol
+from .tnc2 import TNC2Protocol
 
 
 __author__ = "Greg Albrecht W2GMD <oss@undef.net>"  # NOQA pylint: disable=R0801
@@ -76,3 +28,15 @@ __copyright__ = (
 __license__ = "Apache License, Version 2.0"  # NOQA pylint: disable=R0801
 __distribution__ = "kiss3"
 __version__ = version(__distribution__)
+__all__ = [
+    "AbstractKISS",
+    "Address",
+    "Control",
+    "create_serial_connection",
+    "create_tcp_connection",
+    "Frame",
+    "FrameType",
+    "KISSProtocol",
+    "SerialKISS",
+    "TCPKISS",
+]
--- tests/test_kiss_util.py
+++ tests/test_kiss_util.py
@@ -1,9 +1,6 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """Tests for KISS Util Module."""
 
-import kiss3
+from kiss3 import constants, util
 
 __author__ = "Greg Albrecht W2GMD <oss@undef.net>"  # NOQA pylint: disable=R0801
 __copyright__ = (
@@ -16,19 +13,18 @@ def test_escape_special_codes_fend():
     """
     Tests `kiss3.escape_special_codes` util function.
     """
-    assert kiss3.escape_special_codes(kiss3.FEND) == kiss3.FESC_TFEND
+    assert util.escape_special_codes(constants.FEND) == constants.FESC_TFEND
 
 
 def test_escape_special_codes_fesc():
     """
     Tests `kiss3.escape_special_codes` util function.
     """
-    assert kiss3.escape_special_codes(kiss3.FESC) == kiss3.FESC_TFESC
+    assert util.escape_special_codes(constants.FESC) == constants.FESC_TFESC
 
 
 def test_extract_ui(sample_frames):
     """
-    Tests `kiss3.extract_ui` util function.
+    Tests `util.extract_ui` util function.
     """
-    frame_ui = kiss3.extract_ui(sample_frames[0])
-    assert frame_ui == "APRX240W2GMD 6WIDE1 1"
+    assert util.extract_ui(sample_frames[0]) == "APRX240W2GMD 6WIDE1 1"
