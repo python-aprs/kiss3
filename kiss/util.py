a873cd85ddd0d0494f08a64604f7abca563baa93
Masen Furer <m_github@0x26.net>
2022-06-12 15:46:28 -0700

    util: cleanup recover_special_codes, move helpers
    access constants via .constants instead of through
    the module name to avoid any circular import and
    top level name differences.
    
    move attr validation helpers to util
    
    move log helpers to util

--- kiss/util.py
+++ kiss/util.py
@@ -1,13 +1,11 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 """Python KISS Module Utility Functions Definitions."""
+import logging
 
-import kiss
+from . import constants
 
-__author__ = 'Greg Albrecht W2GMD <oss@undef.net>'  # NOQA pylint: disable=R0801
-__copyright__ = 'Copyright 2017 Greg Albrecht and Contributors'  # NOQA pylint: disable=R0801
-__license__ = 'Apache License, Version 2.0'  # NOQA pylint: disable=R0801
+__author__ = "Greg Albrecht W2GMD <oss@undef.net>"
+__copyright__ = "Copyright 2017 Greg Albrecht and Contributors"
+__license__ = "Apache License, Version 2.0"
 
 
 def escape_special_codes(raw_codes):
@@ -19,12 +17,9 @@ def escape_special_codes(raw_codes):
     FESC is then sent as FESC, TFESC."
     - http://en.wikipedia.org/wiki/KISS_(TNC)#Description
     """
-    return raw_codes.replace(
-        kiss.FESC,
-        kiss.FESC_TFESC
-    ).replace(
-        kiss.FEND,
-        kiss.FESC_TFEND
+    return raw_codes.replace(constants.FESC, constants.FESC_TFESC).replace(
+        constants.FEND,
+        constants.FESC_TFEND,
     )
 
 
@@ -37,13 +32,23 @@ def recover_special_codes(escaped_codes):
     replaced by FESC code and FESC_TFEND is replaced by FEND code."
     - http://en.wikipedia.org/wiki/KISS_(TNC)#Description
     """
-    return escaped_codes.replace(
-        kiss.FESC_TFESC,
-        kiss.FESC
-    ).replace(
-        kiss.FESC_TFEND,
-        kiss.FEND
-    )
+    out = bytearray()
+    i = 0
+    while i < len(escaped_codes):
+        if escaped_codes[i] == constants.FESC[0] and i + 1 < len(escaped_codes):
+            if escaped_codes[i + 1] == constants.TFESC[0]:
+                out.append(constants.FESC[0])
+                i += 1  # Skips over the next byte, which would be the TFESC
+            elif escaped_codes[i + 1] == constants.TFEND[0]:
+                out.append(constants.FEND[0])
+                i += 1
+            else:
+                out.append(escaped_codes[i])
+        else:
+            out.append(escaped_codes[i])
+        i += 1
+
+    return out
 
 
 def extract_ui(frame):
@@ -55,10 +60,11 @@ def extract_ui(frame):
     :returns: UI component of frame.
     :rtype: str
     """
-    start_ui = frame.split(
-        b''.join([kiss.FEND, kiss.DATA_FRAME]))
-    end_ui = start_ui[0].split(b''.join([kiss.SLOT_TIME, kiss.UI_PROTOCOL_ID]))
-    return ''.join([chr(x >> 1) for x in end_ui[0]])
+    start_ui = frame.split(b"".join([constants.FEND, constants.DATA_FRAME]))
+    end_ui = start_ui[0].split(
+        b"".join([constants.SLOT_TIME, constants.NO_PROTOCOL_ID]),
+    )
+    return "".join([chr(x >> 1) for x in end_ui[0]])
 
 
 def strip_df_start(frame):
@@ -70,7 +76,7 @@ def strip_df_start(frame):
     :returns: APRS/AX.25 frame sans DATA_FRAME start (0x00).
     :rtype: str
     """
-    return frame.lstrip(kiss.DATA_FRAME).strip()
+    return frame.lstrip(constants.DATA_FRAME).strip()
 
 
 def strip_nmea(frame):
@@ -81,3 +87,55 @@ def strip_nmea(frame):
         if frame[0] == 240:
             return frame[1:].rstrip()
     return frame
+
+
+def getLogger(name):
+    """
+    Get a logger hooked up with the appropriate levels and outputs.
+    """
+    logger = logging.getLogger(name)
+
+    if not logger.handlers:
+        logger.setLevel(constants.LOG_LEVEL)
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(constants.LOG_LEVEL)
+        console_handler.setFormatter(constants.LOG_FORMAT)
+        logger.addHandler(console_handler)
+    return logger
+
+
+def valid_length(at_least, at_most=None, sequence_validator=None):
+    def _validator(instance, attribute, value):
+        if sequence_validator:
+            sequence_validator(instance, attribute, value)
+        len_value = len(value)
+        if len_value < at_least:
+            raise ValueError(
+                "{} must be at least {} (actual={})".format(
+                    attribute.name, at_least, len_value
+                )
+            )
+        if at_most is not None and len_value > at_most:
+            raise ValueError(
+                "{} must be at most {} (actual={})".format(
+                    attribute.name, at_most, len_value
+                )
+            )
+
+    return _validator
+
+
+def instance_of_or(types, other_validator):
+    """check if the value is a bool or satisfies `other_validator`."""
+
+    def _validator(instance, attribute, value):
+        if not isinstance(value, types):
+            other_validator(instance, attribute, value)
+
+    return _validator
+
+
+def optional_bool_or_bytes(v):
+    if v is not None and not isinstance(v, bool):
+        return bytes(v)
+    return v
