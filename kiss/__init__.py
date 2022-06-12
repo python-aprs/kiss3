acd8d710dbb5e28a03f292d22e01ee9c5bfbec1b
Masen Furer <m_github@0x26.net>
2022-06-12 15:52:24 -0700

    __init__: establish public API
    expose specific items as part of the public API

--- kiss/__init__.py
+++ kiss/__init__.py
@@ -1,8 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-# Python KISS Module.
-
 """
 Python KISS Module.
 ~~~~
@@ -12,25 +7,32 @@ Python KISS Module.
 :copyright: Copyright 2017 Greg Albrecht and Contributors
 :license: Apache License, Version 2.0
 :source: <https://github.com/ampledata/kiss>
-
 """
+from importlib_metadata import version
 
-from .constants import (LOG_FORMAT, LOG_LEVEL, SERIAL_TIMEOUT, READ_BYTES,  # NOQA
-                        FEND, FESC, TFEND, TFESC, FESC_TFEND, FESC_TFESC,
-                        DATA_FRAME, TX_DELAY, PERSISTENCE, SLOT_TIME, TX_TAIL,
-                        FULL_DUPLEX, SET_HARDWARE, RETURN, DATAFRAME, TXDELAY,
-                        P, SLOTTIME, TXTAIL, FULLDUPLEX, SETHARDWARE,
-                        DEFAULT_KISS_CONFIG_VALUES, KISS_ON, KISS_OFF,
-                        NMEA_HEADER, UI_PROTOCOL_ID)
-
-from .exceptions import SocketClosetError  # NOQA
-
-from .util import (escape_special_codes, recover_special_codes, extract_ui,  # NOQA
-                   strip_df_start, strip_nmea)
-
-from .classes import KISS, TCPKISS, SerialKISS  # NOQA
+from .classes import SerialKISS, TCPKISS
+from .kiss import (
+    create_serial_connection,
+    create_tcp_connection,
+    Command,
+    KISSDecode,
+    AX25KISSDecode,
+    KISSProtocol,
+)
 
 
-__author__ = 'Greg Albrecht W2GMD <oss@undef.net>'  # NOQA pylint: disable=R0801
-__copyright__ = 'Copyright 2017 Greg Albrecht and Contributors'  # NOQA pylint: disable=R0801
-__license__ = 'Apache License, Version 2.0'  # NOQA pylint: disable=R0801
+__author__ = "Greg Albrecht W2GMD <oss@undef.net>"
+__copyright__ = "Copyright 2017 Greg Albrecht and Contributors"
+__license__ = "Apache License, Version 2.0"
+__distribution__ = "kiss"
+__version__ = version(__distribution__)
+__all__ = [
+    "AX25KISSDecode",
+    "create_serial_connection",
+    "create_tcp_connection",
+    "Command",
+    "KISSDecode",
+    "KISSProtocol",
+    "SerialKISS",
+    "TCPKISS",
+]
